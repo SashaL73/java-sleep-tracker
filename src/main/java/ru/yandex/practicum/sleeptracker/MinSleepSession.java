5a8d8368e77015a490207c601eaa2ea419415eda
Lebedenko Aleksander <lebedenko.aleksander2015@yandex.ru>
2025-11-29 22:38:10 +0300

    Создан класс функция подсчета самого короткого сна.

---
+++ src/main/java/ru/yandex/practicum/sleeptracker/MinSleepSession.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.sleeptracker;
+
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class MinSleepSession implements Function<List<SleepingSession>, SleepAnalysisResult> {
+
+    @Override
+    public SleepAnalysisResult apply(List<SleepingSession> sleepingSessions) {
+
+        SleepingSession minSession = sleepingSessions.stream()
+                .min(Comparator.comparing(s -> Duration.between(s.startSleep, s.endSleep)))
+                .orElse(null);
+        Duration minDuration = Duration.between(minSession.startSleep, minSession.endSleep);
+        int min = Math.toIntExact(minDuration.toMinutes());
+
+
+        return new SleepAnalysisResult("минимальная сессия в минутах", min);
+    }
+}
