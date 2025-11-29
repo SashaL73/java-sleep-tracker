08ddc580965ada2bcc470d6ab2047ebe22b7368e
Lebedenko Aleksander <lebedenko.aleksander2015@yandex.ru>
2025-11-29 22:51:11 +0300

    Отредактирован правильный перенос точки

--- src/main/java/ru/yandex/practicum/sleeptracker/UserClassification.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/UserClassification.java
@@ -17,10 +17,10 @@ public class UserClassification implements Function<List<SleepingSession>, Sleep
 
         List<SleepingSession> sessions = sleepingSessions.stream()
                 .filter(s -> s.endSleep.toLocalDate().equals(s.startSleep.toLocalDate().plusDays(1))
-                        || s.startSleep.isBefore(LocalDateTime.
-                        of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 6, 0))
-                        && s.endSleep.toLocalDate().isBefore(LocalDate.
-                        of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth() + 1)))
+                        || s.startSleep.isBefore(LocalDateTime
+                        .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 6, 0))
+                        && s.endSleep.toLocalDate().isBefore(LocalDate
+                        .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth() + 1)))
                 .toList();
 
         List<SleepingSession> s1 = sessions.stream()
@@ -30,10 +30,10 @@ public class UserClassification implements Function<List<SleepingSession>, Sleep
                             || s.startSleep.toLocalTime().isBefore(LocalTime.of(6, 0))
                             && s.endSleep.toLocalTime().isAfter(LocalTime.of(9, 0))) {
                         count1.getAndIncrement();
-                    } else if (s.startSleep.isBefore(LocalDateTime.
-                            of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 22, 0))
-                            && s.endSleep.isBefore(LocalDateTime.
-                            of(s.endSleep.getYear(), s.endSleep.getMonth(), s.endSleep.getDayOfMonth(), 7, 0))) {
+                    } else if (s.startSleep.isBefore(LocalDateTime
+                            .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 22, 0))
+                            && s.endSleep.isBefore(LocalDateTime
+                            .of(s.endSleep.getYear(), s.endSleep.getMonth(), s.endSleep.getDayOfMonth(), 7, 0))) {
                         count2.getAndIncrement();
                     }
                 })
