f14c5abb6c18048668cc43935bad2c358ed23772
Lebedenko Aleksander <lebedenko.aleksander2015@yandex.ru>
2025-11-30 21:33:02 +0300

    Исправлена логика (.isBefore(s.startSleep.toLocalDate().plusDays(1))). Добавлена провера на пустой список.

--- src/main/java/ru/yandex/practicum/sleeptracker/NightSleepAnalyzer.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/NightSleepAnalyzer.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.sleeptracker;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Period;
@@ -11,34 +10,38 @@ public class NightSleepAnalyzer implements Function<List<SleepingSession>, Sleep
 
     @Override
     public SleepAnalysisResult apply(List<SleepingSession> sleepingSessions) {
-        List<SleepingSession> sessions = sleepingSessions.stream()
-                .filter(s -> s.endSleep.toLocalDate().equals(s.startSleep.toLocalDate().plusDays(1))
-                        || s.startSleep.isBefore(LocalDateTime
-                        .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 6, 0))
-                        && s.endSleep.toLocalDate().isBefore(LocalDate
-                        .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth() + 1)))
-                .toList();
 
-        LocalDateTime startDate = sleepingSessions.getFirst().getStartSleep();
-        LocalDateTime endDate = sleepingSessions.getLast().getEndSleep();
-        long nightCount = 0;
-        long counterSleepless = 0;
+        if (sleepingSessions != null && !sleepingSessions.isEmpty()) {
+            List<SleepingSession> sessions = sleepingSessions.stream()
+                    .filter(s -> s.endSleep.toLocalDate().equals(s.startSleep.toLocalDate().plusDays(1))
+                            || s.startSleep.isBefore(LocalDateTime
+                            .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 6, 0))
+                            && s.endSleep.toLocalDate().isBefore(s.startSleep.toLocalDate().plusDays(1)))
+                    .toList();
 
-        if (sleepingSessions.getFirst().startSleep.toLocalTime().isAfter(LocalTime.of(12, 0))) {
-            Period period = Period.between(startDate.toLocalDate(), endDate.toLocalDate());
-            nightCount = period.getDays();
-        } else if (sleepingSessions.getFirst().startSleep.toLocalTime().isBefore(LocalTime.of(12, 0))) {
-            Period period = Period.between(startDate.toLocalDate().minusDays(1), endDate.toLocalDate());
-            nightCount = period.getDays();
-        }
+            LocalDateTime startDate = sleepingSessions.getFirst().getStartSleep();
+            LocalDateTime endDate = sleepingSessions.getLast().getEndSleep();
+            long nightCount = 0;
+            long counterSleepless = 0;
 
-        if (nightCount >= 1 && (!sessions.isEmpty())) {
-            long sleeping = sessions.size();
-            counterSleepless = nightCount - sleeping;
+            if (sleepingSessions.getFirst().startSleep.toLocalTime().isAfter(LocalTime.of(12, 0))) {
+                Period period = Period.between(startDate.toLocalDate(), endDate.toLocalDate());
+                nightCount = period.getDays();
+            } else if (sleepingSessions.getFirst().startSleep.toLocalTime().isBefore(LocalTime.of(12, 0))) {
+                Period period = Period.between(startDate.toLocalDate().minusDays(1), endDate.toLocalDate());
+                nightCount = period.getDays();
+            }
+
+            if (nightCount >= 1 && (!sessions.isEmpty())) {
+                long sleeping = sessions.size();
+                counterSleepless = nightCount - sleeping;
+            } else {
+                counterSleepless = nightCount;
+            }
+
+            return new SleepAnalysisResult("количество бессонных ночей", counterSleepless);
         } else {
-            counterSleepless = nightCount;
+            return new SleepAnalysisResult("количество бессонных ночей", "нет данных");
         }
-
-        return new SleepAnalysisResult("количество бессонных ночей", counterSleepless);
     }
 }
--- src/main/java/ru/yandex/practicum/sleeptracker/UserClassification.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/UserClassification.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.sleeptracker;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
@@ -11,47 +10,53 @@ public class UserClassification implements Function<List<SleepingSession>, Sleep
 
     @Override
     public SleepAnalysisResult apply(List<SleepingSession> sleepingSessions) {
-        AtomicInteger counterOwl = new AtomicInteger();
-        AtomicInteger counterLark = new AtomicInteger();
-        AtomicInteger counterPigeon = new AtomicInteger();
-        String userClassification = "";
 
-        List<SleepingSession> sessions = sleepingSessions.stream()
-                .filter(s -> s.endSleep.toLocalDate().equals(s.startSleep.toLocalDate().plusDays(1))
-                        || s.startSleep.isBefore(LocalDateTime
-                        .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 6, 0))
-                        && s.endSleep.toLocalDate().isBefore(LocalDate
-                        .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth() + 1)))
-                .toList();
+        if (sleepingSessions != null && !sleepingSessions.isEmpty()) {
+            AtomicInteger counterOwl = new AtomicInteger();
+            AtomicInteger counterLark = new AtomicInteger();
+            AtomicInteger counterPigeon = new AtomicInteger();
+            String userClassification = "";
 
-        List<SleepingSession> s1 = sessions.stream()
-                .peek(s -> {
-                    if (s.startSleep.toLocalTime().isAfter(LocalTime.of(23, 0))
-                            && s.endSleep.toLocalTime().isAfter(LocalTime.of(9, 0))
-                            || s.startSleep.toLocalTime().isBefore(LocalTime.of(6, 0))
-                            && s.endSleep.toLocalTime().isAfter(LocalTime.of(9, 0))) {
-                        counterOwl.getAndIncrement();
-                    } else if (s.startSleep.isBefore(LocalDateTime
-                            .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 22, 0))
-                            && s.endSleep.isBefore(LocalDateTime
-                            .of(s.endSleep.getYear(), s.endSleep.getMonth(), s.endSleep.getDayOfMonth(), 7, 0))) {
-                        counterLark.getAndIncrement();
-                    } else {
-                        counterPigeon.getAndIncrement();
-                    }
-                })
-                .toList();
+            List<SleepingSession> sessions = sleepingSessions.stream()
+                    .filter(s -> s.endSleep.toLocalDate().equals(s.startSleep.toLocalDate().plusDays(1))
+                            || s.startSleep.isBefore(LocalDateTime
+                            .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(), 6, 0))
+                            && s.endSleep.toLocalDate().isBefore(s.startSleep.toLocalDate().plusDays(1)))
+                    .toList();
 
-        if (counterPigeon.get() > counterLark.get() && counterPigeon.get() > counterOwl.get()) {
-            userClassification = "Голубь";
-        } else if (counterOwl.get() == counterLark.get()) {
-            userClassification = "Голубь";
-        } else if (counterLark.get() > counterOwl.get()) {
-            userClassification = "Жаворонок";
-        } else if (counterOwl.get() > counterLark.get()) {
-            userClassification = "Сова";
+            List<SleepingSession> s1 = sessions.stream()
+                    .peek(s -> {
+                        if (s.startSleep.toLocalTime().isAfter(LocalTime.of(23, 0))
+                                && s.endSleep.toLocalTime().isAfter(LocalTime.of(9, 0))
+                                || s.startSleep.toLocalTime().isBefore(LocalTime.of(6, 0))
+                                && s.endSleep.toLocalTime().isAfter(LocalTime.of(9, 0))) {
+                            counterOwl.getAndIncrement();
+                        } else if (s.startSleep.isBefore(LocalDateTime
+                                .of(s.startSleep.getYear(), s.startSleep.getMonth(), s.startSleep.getDayOfMonth(),
+                                        22, 0))
+                                && s.endSleep.isBefore(LocalDateTime
+                                .of(s.endSleep.getYear(), s.endSleep.getMonth(), s.endSleep.getDayOfMonth(),
+                                        7, 0))) {
+                            counterLark.getAndIncrement();
+                        } else {
+                            counterPigeon.getAndIncrement();
+                        }
+                    })
+                    .toList();
+
+            if (counterPigeon.get() > counterLark.get() && counterPigeon.get() > counterOwl.get()) {
+                userClassification = "Голубь";
+            } else if (counterOwl.get() == counterLark.get()) {
+                userClassification = "Голубь";
+            } else if (counterLark.get() > counterOwl.get()) {
+                userClassification = "Жаворонок";
+            } else if (counterOwl.get() > counterLark.get()) {
+                userClassification = "Сова";
+            }
+
+            return new SleepAnalysisResult("Вы", userClassification);
+        } else {
+            return new SleepAnalysisResult("Вы", "нет данных");
         }
-
-        return new SleepAnalysisResult("Вы", userClassification);
     }
 }
